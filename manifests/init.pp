68307fb8c9bbfc4191810f92c77ac8d532c1447c
Martin Alfke <tuxmea@users.noreply.github.com>
2020-11-02 12:49:14 +0100

    Update init.pp
    Add parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,13 @@
 #
 # @example
 #   include project_d
-class demo_module {
-  $bool = true
+class demo_module (
+  Boolean   $bool = true,
+  String[1] $string = 'foo',
+){
   $integer_1 = project_d::any2int($bool)
   notify { "Integer 1: ${integer_1}": }
 
-  $string = 'foo'
   $integer_2 = project_d::any2int($string)
   notify { "Integer 2: ${integer_2}": }
 
