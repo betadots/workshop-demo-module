a703a27f41d7e772bf797401ecd1a5e8d0da8ac7
Tim <tim@bastelfreak.de>
2021-09-23 14:23:26 +0200

    add Plan for last assignment

---
+++ workshop_plans/itzbund.pp
@@ -0,0 +1,25 @@
+plan demo::itzbund (
+  TargetSpec $servergruppe_1,
+  TargetSpec $servergruppe_2,
+  TargetSpec $servergruppe_3,
+  String[1]  $dateiinhalt,
+  Stdlib::Absolutepath $pfad,
+){
+  $version = run_task('enterprise_tasks::get_agent_version', $servergruppe_1)
+  out::message($version)
+
+  $kernel = run_task('facter_task', $servergruppe_2, 'fact' => 'kernel')
+  out::message($kernel)
+
+  # https://puppet.com/docs/bolt/latest/plan_functions.html#write-file
+  # https://puppet.com/docs/pe/2019.8/plans_limitations.html
+  # write_file ist in PE Plans nicht verfügbar
+  #write_file($dateiinhalt, $pfad, $servergruppe_3)
+
+  # https://puppet.com/docs/bolt/latest/applying_manifest_blocks.html#applying-manifest-blocks-from-a-puppet-plan
+  apply($servergruppe_3, _catch_errors => true, _noop => false,) {
+    file { $pfad:
+      content => $dateiinhalt,
+    }
+  }
+}
