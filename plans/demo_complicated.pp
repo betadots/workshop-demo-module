d45a2ebb4e9eda6920e6a048ecf4b4ae30f16362
Tim Meusel <tim@bastelfreak.de>
2023-05-19 12:14:51 +0200

    Add another demo plan for trainings

---
+++ plans/demo_complicated.pp
@@ -0,0 +1,51 @@
+#
+# @summary workshop demo plan to cal a few tasks
+#
+# @param puppet_agent_group desired list of nodes where we collect the puppet agent version
+# @param kernelversion_group node list where we will collect the fact
+# @param file_group where we will create the awesome new file
+# @param file_path the path to the awesome file
+# @param file_content the content of the awesome file
+# @param file_ensure desired state for awesome file
+#
+# @author <tim.meusel@betadots.de>
+plan app1_example::workshop_plan (
+  TargetSpec $puppet_agent_group,
+  TargetSpec $kernelversion_group,
+  TargetSpec $file_group,
+  Stdlib::Absolutepath $file_path = '/tmp/spirit',
+  String[1] $file_content = 'Hi!',
+  Enum['file', 'absent'] $file_ensure = 'file',
+) {
+  $agent_results = run_task(
+    'enterprise_tasks::get_agent_version',
+    $puppet_agent_group,
+    'Gather the Puppet Agent version',
+  )
+  # simple output
+  # out::message($agent_results)
+
+  # more beautiful:
+  # https://www.puppet.com/docs/bolt/latest/bolt_types_reference.html#resultset
+  if $agent_results.ok {
+    $versions = $agent_results.results.map |$agent_result| { "${agent_result.target}: ${agent_result.value['version']}" }
+    out::message("Puppet versions are: ${versions}")
+  }
+
+  $kernel_results = run_task( 'facter_task', $kernelversion_group, 'Get the kernelversion fact', { 'fact' => 'kernelversion', })
+  #out::message($kernel_results)
+  $output = $kernel_results.results.map |$kernel_result| { $kernel_result.value['kernelversion'] }
+  out::message(join($output, ', '))
+
+  apply($file_group, '_description' => "manage ${file_path}") {
+    file { $file_path:
+      ensure  => $file_ensure,
+      content => "${file_content}\n",
+    }
+  }
+
+  $nodes_from_puppetdb = puppetdb_query('inventory[certname] {trusted.extensions.pp_role = "appserver"}')
+  $target_hosts = $nodes_from_puppetdb.map |$r| { $r["certname"] }
+  $plan_output = run_plan('enterprise_tasks::get_service_status', { 'service' => 'pxp-agent', 'target' => $target_hosts })
+  out::message($plan_output)
+}
